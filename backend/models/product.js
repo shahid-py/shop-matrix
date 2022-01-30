b18044ada901936f287b96c5df9532b63f141782
Mohd Shahid Khan <khan42290@gmail.com>
2022-01-30 15:30:55 +0530

    fix : deleted error handler from product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -24,16 +24,6 @@ const ProductSchema = new Schema({
 
     const Product = mongoose.model("Product", ProductSchema);
 
-async function seedAllProducts() {
-  try {
-    products.forEach(async (product) => {
-      const addedProduct = new Product(product);
-      await addedProduct.save();
-    });
-    console.log("products added successfully")
-  } catch (error) {
-    console.error(error)
-  }
-}
+
 
 module.exports =  { Product, seedAllProducts }
