133ed62027e0599a98ea36770e9ecee6805fc7d1
Mohd Shahid Khan <khan42290@gmail.com>
2022-01-30 17:02:10 +0530

    feat  : build frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,19 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-     hello world
+  return <div className="app">
+
+     {/*Navbar*/}
+     {/* sidebar */}
+     {/* backdrop */}
+     {/* homescreen*/}
+     {/* productscreen */}
+     {/* cartscreen */}
+
+
+
     </div>
-  );
+  ;
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+          <div className="navbar__logo">
+            <h2>MATRIX</h2>
+          </div>
+    
+          <ul className="navbar__links">
+            <li>
+              <Link to="/cart" className="cart__link">
+                <i className="fas fa-shopping-cart"></i>
+                <span>
+                  Cart <span className="cartlogo__badge">{getCartCount()}</span>
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Shop</Link>
+            </li>
+          </ul>
+    
+          <div className="hamburger__menu" onClick={click}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </nav>
+      );
+    };
+    
+    export default Navbar;
