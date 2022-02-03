f0cdf1ded201c81a3d0f63b3aeb5cf2b024848de
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-03 21:22:58 +0530

    feat: setting up redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,17 @@
 import { createStore, combineReducers, applyMiddleware,  } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+// Reducers
+import { cartReducer } from "./reducers/cartReducers";
+import {
+    getProductsReducer,
+    getProductDetailsReducer,
+  } from "./reducers/productReducers";
+  
 const reducer =combineReducers({
-
+cart: cartReducer,
+getProducts: getProductsReducer,
+  getProductDetails: getProductDetailsReducer,
 });
 
 const middleware=[thunk];
