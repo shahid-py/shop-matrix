f634256e36cb38ad20208aedb136366f9dd00269
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-03 21:20:40 +0530

    feat: integrating product from data base by fetching api

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,23 @@
 import './Product.css';
 import { Link } from 'react-router-dom';
-const Product = () => {
-  return <div className='product'>
-      <img src= "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80" alt=''/>
-      <div className='product__info'>
+const Product = ({ imageUrl, description, price, name, productId }) => {
+  return (
+    <div className="product">
+      <img src={imageUrl} alt={name} />
 
-     <p className='info__name'>Product info</p>
-     <p className='info__description'>
-     Voluptate consectetur exercitation qui anim magna qui quis tempor. Ea ipsum anim eiusmod do cillum culpa veniam excepteur quis duis. Reprehenderit elit occaecat cupidatat reprehenderit.
-     </p>
-     <p>
-         rs499.99
-     </p>
-     <Link to={`/product/${1111}`} className='info__button'>View</Link>
+      <div className="product__info">
+        <p className="info__name">{name}</p>
+
+        <p className="info__description">{description.substring(0, 100)}...</p>
+
+        <p className="info__price">Rs. {price} /-</p>
+
+        <Link to={`/product/${productId}`} className="info__button">
+          View
+        </Link>
       </div>
-  </div>;
+  </div>
+  );
 };
 
 export default Product;
