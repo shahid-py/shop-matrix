3a23c4c6978a298fcd4d1156478d1f3b9828190a
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-03 21:19:54 +0530

    fix: integrating data pre fixing

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
