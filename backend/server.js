8e45b346ce11452dc871b412e83019ba4d7caa54
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-03 19:12:22 +0530

    refactor

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const express = require('express');
-const connectDB = require('./config/db');
-const productRoutes =require('./routes/productRoutes');
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
 
 connectDB();
 
@@ -9,8 +9,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/products',productRoutes)
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT} `))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
