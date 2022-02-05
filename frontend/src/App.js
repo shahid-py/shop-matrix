e964afc4c1e9b5f562c6a7c34a7b9ed266109140
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-05 13:26:16 +0530

    refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 
-import './App.css';
+
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
 
 
 // Components
@@ -30,10 +32,10 @@ function App() {
       <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
  
 <main>
-  <Routes>
-  <Route path="/" element={<HomeScreen/>} />
-          <Route path="/product/:id" element={<ProductScreen/>} />
-          <Route path="/cart" element={<CartScreen/>} />
+     <Routes>
+        <Route  path="/" element={<HomeScreen/>} />
+          <Route  path="/product/:id" element={<ProductScreen/>} />
+          <Route  path="/cart" element={<CartScreen/>} />
         </Routes>
       </main>
     </Router>
