0e305caa792f83e8376f8d33ff795dca0d0fba72
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-08 21:53:45 +0530

    feat : Razorpay payment method

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,70 @@
 require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
+const app = express();
 const connectDB = require("./config/db");
+const { nanoid } = require("nanoid");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { request } = require("http");
 
+//initializing db connection
 connectDB();
 
-const app = express();
-
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/products", productRoutes);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
-app.use("/api/products", productRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_6nxP2p2M8T5UeD",
+  key_secret: "9N6TTlgJHptWiVMM3eUZgayh",
+});
+app.post("/razorpay", async (req, res) => {
+  try {
+  
+  const {amount }= req.body;
+  
+
+  const options = {
+    amount: amount * 100,
+    currency:'INR',
+    receipt: nanoid(),
+    
+  };
+  const response = await razorpay.orders.create(options);
+  console.log(response);
+  
+  res.json({ success: true, orderId: response.id, amount: response.amount, currency: response.currency })
+} catch (error) {
+  res.json({ success: false, errorMessage: error.message })
+}
+})
+
+app.post("/verification", (req, res) => {
+  //do a validation
+  const secret = "12345678";
+  const { orderId, paymentId, signature } = req.body;
+  const crypto = require("crypto");
+  const shasum = crypto.createHmac("sha256", secret);
+  shasum.update(`${orderId}|${paymentId}`);
+  const digest = shasum.digest("hex");
+  if (digest !== signature) {
+    return res.json({ success: false, message: "transaction failed!" })
+  }
+
+  res.json({ success: true, message: "order successfully placed!" });
+
+
+});
+
+
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,6 @@
 import './CartScreen.css'
-
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -10,13 +11,29 @@ import CartItem from '../components/CartItem';
 
 import {addToCart, removeFromCart} from  '../redux/actions/cartActions';
 
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
+const __DEV__ =document.domain === 'localhost'
+
+
+
 const CartScreen = () => {
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  
-
+  useEffect(() => {}, []);
   const qtyChangeHandler =(id, qty) => {
     dispatch(addToCart(id, qty))
   }
@@ -26,15 +43,71 @@ const CartScreen = () => {
   }
 
   const getCartCount= () =>{
-    return cartItems.reduce((qty, item) => Number( item.qty) +qty, 0 );
+    return cartItems.reduce((qty, item) => ( item.qty) +qty, 0 );
   }
 
   const getCartSubTotal = () => {
-    return cartItems
-      .reduce((price, item) => price + item.price * item.qty, 0)
-      .toFixed(2);
+    return cartItems.reduce((price, item) =>+price + +item.price * +item.qty, 0)
+      
   };
 
+  async function displayRazorpay() {
+
+    const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+    
+
+    const payment_response = await axios.post(`http://localhost:5000/razorpay`,{
+      amount:getCartSubTotal()
+  })
+
+    if(!res) {
+      alert('Razorpay SDK failed to load')
+      return;
+    }
+    const { amount, currency, orderId } = payment_response.data;
+    const options = {
+      key: __DEV__ ? 'rzp_test_6nxP2p2M8T5UeD'  : 'API_NOT_AVAILABLE',
+      currency: currency,
+      amount:amount,
+      order_id: orderId,
+      name: "MATRIX SHOPPING",
+      description: "MATRIX Transaction",
+      image: "https://razorpay.com/assets/razorpay-glyph.svg",
+       
+      handler: async function (response){
+        const paymentVerification = await axios.post(
+          'http://localhost:5000/razorpay/verification',
+          {
+            orderId: orderId,
+            paymentId: response.razorpay_payment_id,
+            signature: response.razorpay_signature,
+          }
+        );
+
+        if (paymentVerification.data.success) {
+          alert(paymentVerification.data.message);
+          removeHandler()
+          
+        } else {
+          alert("something went wrong");
+        }
+      },
+      prefill: {
+          name: "tester",
+          email: "test@gmail.com",
+        contact: "1234567890",
+      },
+      
+      theme: {
+          color: "#3399cc"
+      }
+  }
+  const paymentObject = new  window.Razorpay(options);
+  paymentObject.open();
+  }
+  
+
+ 
 
 
   return(
@@ -61,14 +134,15 @@ const CartScreen = () => {
       <div className="cartscreen__right">
         <div className="cartscreen__info">
           <p>subtotal({getCartCount()}) items</p>
-          <p>Rs.{getCartSubTotal()}/-</p>
+          <p>Rs. {getCartSubTotal()}</p>
         </div>
        <div>
-         <button>Proceed To Checkout</button>
+       <button onClick={displayRazorpay} >Proceed To Checkout</button>
        </div>
       </div>
   </div>
   );
 };
-
 export default CartScreen;
+
+
