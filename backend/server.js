d5f0ae37a08dd55bec4c8f36a550b6e91a642875
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-10 12:23:41 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-
-const path =require("path");
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const app = express();
@@ -18,21 +16,21 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/products", productRoutes);
 app.use(express.json());
-if(process.env.NODE_ENV ==="production") {
-  app.use(express.static(path.join(__dirname, '/cd../frontend/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'cd..' ,'frontend','build', 'index.html'));
-})
 
-} else {
-   app.get('/', (req, res) => {
-     res.send('api running');
-   })
-}
-
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "API running..." });
 });
+
+app.use(express.static(path.join(__dirname, '/cd../frontend/build')))
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'cd..' ,'frontend','build', 'index.html'),
+    function(err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+})
 
 
 const PORT = process.env.PORT || 5000;
